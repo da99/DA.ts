b6c9ff4ead8a4d965a4f72c332fb2fccbcca6d88
da99 <1551848+da99@users.noreply.github.com>
2022-04-26 08:36:23 -0500

    Fixed: update spec/

--- templates/bin_update.sh
+++ templates/bin_update.sh
@@ -2,9 +2,11 @@
 
 set -u -e -o pipefail
 
-da.ts build update "$(bin/config all)"
+if test -d node_modules ; then
+  npm update
+fi # if
 
-for x in $(find src -type f -name '*.ts') ; do
-  echo "deno cache --reload "$x""
+for x in $(find bin src spec -type f -name '*.ts') ; do
+  echo "=== deno cache --reload "$x""
   deno cache --reload "$x"
 done
